445f0f159fb623aa54d07b5fc35935386464a20d
Alex <lexakorh@gmail.com>
2018-10-17 08:29:50 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { share } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+
+    ngOnInit() {
+        const obs = Observable.create(observer => {
+            setInterval(() => observer.next(Date.now()), 1000);
+        })
+            .pipe(
+                share()
+            );
+
+        obs.subscribe(v => console.log('1st subscriber: ' + v));
+
+        setTimeout(() => {
+            obs.subscribe(v => console.log('2nd subscriber: ' + v);
+        }, 1000);
+    }
 }
